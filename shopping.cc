ecbe3ea4a005d4fac78096f319e8507a1362340b
jammer6699 <51632407+jammer6699@users.noreply.github.com>
2019-06-12 15:52:25 +0900

    Update shopping.cc

--- shopping.cc
+++ shopping.cc
@@ -1,34 +1,36 @@
 #include <iostream>
 #include "./shopping.h"
 
-// Push Goods object to shopping_list_ vector
-void Shopping::PushList(Goods object) {
-  shopping_list_.push_back(object);
+void GoodsList::Add(Goods object) {
+  std::vector<Goods>::iterator i = std::find(list_.begin(), list_.end(), object);
+  if (i == list_.end())  // object not in the list
+  list_.push_back(object);
 }
 
-// Get Goods object from end of shopping_list_ vector
-Goods Shopping::GetLastElement() {
-  return shopping_list_.back();
+void GoodsList::Remove(Goods object) {
+	std::vector<Goods>::iterator i = std::find(list_.begin(), list_.end(), object);
+	if (i == list_.end())  // object not in the list
+		list_.erase(i);
 }
 
-// Print shopping_list_ Elements
-void Shopping::PrintList() {
-  for (std::vector<Goods>::iterator i = shopping_list_.begin();
-      i != shopping_list_.end(); i++) {
+
+
+void GoodsList::Print() {
+  for (std::vector<Goods>::iterator i = list_.begin();
+      i != list_.end(); i++) {
     std::cout << "Goods #" << i->GetIndex() << " - Price : " <<
     i->GetPrice() << std::endl;
   }
 }
 
-// Sort shopping_list_ Elements by Price
-// Apply Selection Sort algorithm
-void Shopping::SortByPrice() {
-  std::vector<Goods>::iterator p = shopping_list_.begin();
-  std::vector<Goods>::iterator min = shopping_list_.end();
-  for (std::vector<Goods>::iterator i = shopping_list_.begin();
-      i != shopping_list_.end()-1; i++) {
-    for (std::vector<Goods>::iterator j = i; j != shopping_list_.end(); j++) {
-      if (min == shopping_list_.end()) {
+
+void GoodsList::SortByPrice() {
+  std::vector<Goods>::iterator p = list_.begin();
+  std::vector<Goods>::iterator min = list_.end();
+  for (std::vector<Goods>::iterator i = list_.begin();
+      i != list_.end()-1; i++) {
+    for (std::vector<Goods>::iterator j = i; j != list_.end(); j++) {
+      if (min == list_.end()) {
         min = j;
         continue;
       }
@@ -37,19 +39,18 @@ void Shopping::SortByPrice() {
       }
     }
     std::iter_swap(i, min);
-    min = shopping_list_.end();
+    min = list_.end();
   }
 }
 
-// Sort shopping_list_ Elements by Index
-// Apply Selection Sort algorithm
-void Shopping::SortByIndex() {
-  std::vector<Goods>::iterator p = shopping_list_.begin();
-  std::vector<Goods>::iterator min = shopping_list_.end();
-  for (std::vector<Goods>::iterator i = shopping_list_.begin();
-      i != shopping_list_.end()-1; i++) {
-    for (std::vector<Goods>::iterator j = i; j != shopping_list_.end(); j++) {
-      if (min == shopping_list_.end()) {
+
+void GoodsList::SortByIndex() {
+  std::vector<Goods>::iterator p = list_.begin();
+  std::vector<Goods>::iterator min = list_.end();
+  for (std::vector<Goods>::iterator i = list_.begin();
+      i != list_.end()-1; i++) {
+    for (std::vector<Goods>::iterator j = i; j != list_.end(); j++) {
+      if (min == list_.end()) {
         min = j;
         continue;
       }
@@ -58,6 +59,6 @@ void Shopping::SortByIndex() {
       }
     }
     std::iter_swap(i, min);
-    min = shopping_list_.end();
+    min = list_.end();
   }
 }
